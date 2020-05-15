7bdbcbc008c7a334754da9a2543274ea2ac3d70b
Berthelmaster <berthel0802@gmail.com>
2020-05-15 20:14:50 +0200

    template added

---
+++ TemplateStockExchange/TemplateStockExchange/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TemplateStockExchange.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+
+        private void seedData(ModelBuilder builder)
+        {
+
+        }
+
+    }
+}
