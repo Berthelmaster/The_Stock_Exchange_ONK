ba991f2118da0d5ea542e6c13579dfff62569c09
Berthelmaster <berthel0802@gmail.com>
2020-05-17 12:28:51 +0200

    user and Stock added to all projects

---
+++ TemplateStockExchange/AvailableStocksBroker/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AvailableStocksBroker.Models
+{
+    public class User
+    {
+        public User(int id, string name, string email, string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public User(int id, string name, string email, string password, List<Stock> stocks)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            Stocks = stocks;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Stock> Stocks { get; set; }
+
+       
+    }
+}
---
+++ TemplateStockExchange/UserStocksBroker/Models/Stock.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserStocksBroker.Models
+{
+    public class Stock
+    {
+
+        public Stock(int id, int price, string name, int quantity, string seller, string buyer, DateTime timeStamp, User user)
+        {
+            Id = id;
+            Price = price;
+            Name = name;
+            Quantity = quantity;
+            Seller = seller;
+            Buyer = buyer;
+            TimeStamp = timeStamp;
+            User = user;
+        }
+
+        public Stock(int id, int price, string name, int quantity, string seller, string buyer, DateTime timeStamp)
+        {
+            Id = id;
+            Price = price;
+            Name = name;
+            Quantity = quantity;
+            Seller = seller;
+            Buyer = buyer;
+            TimeStamp = timeStamp;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Seller { get; set; }
+        public string Buyer { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public User User { get; set; }
+
+    }
+}
