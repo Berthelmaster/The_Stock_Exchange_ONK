fdc00b8c4f59c3d60477b5182a2f1f1ff89c8214
Berthelmaster <berthel0802@gmail.com>
2020-05-21 16:17:27 +0200

    frondend progress

--- TemplateStockExchange/frontend/src/main.js
+++ TemplateStockExchange/frontend/src/main.js
@@ -1,8 +1,30 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueRouter from "vue-router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import Login from "./components/Login";
+import Mainpage from "./components/Mainpage";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/', component: Login
+    },
+    {
+      path: '/mainpage', component: Mainpage
+    },
+  ],
+  mode: 'history'
+});
+
 
 new Vue({
+  router,
+
   render: h => h(App)
 }).$mount("#app");
